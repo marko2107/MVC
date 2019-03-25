5536d6fd75c9c3dd0e3b46fdb2028868b1ed7c48
Marko Drnasin <marko.drnasin@gmail.com>
2019-03-25 16:51:01 +0100

    zavrseni regular ekxpressions

--- Core/Router.php
+++ Core/Router.php
@@ -28,8 +28,20 @@ class Router
      *
      * @return void
      */
-    public function add($route, $params)
+    public function add($route, $params = [])
     {
+        // Convert the route to a regular expression: escape forward slashes
+        $route = preg_replace('/\//', '\\/', $route);
+
+        // Convert variables e.g. {controller}
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+
+        // Convert variables with custom regular expressions e.g. {id:\d+}
+        $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+
+        // Add start and end delimiters, and case insensitive flag
+        $route = '/^' . $route . '$/i';
+
         $this->routes[$route] = $params;
     }
 
@@ -53,30 +65,17 @@ class Router
      */
     public function match($url)
     {
-        /*
         foreach ($this->routes as $route => $params) {
-            if ($url == $route) {
+            if (preg_match($route, $url, $matches)) {
+                foreach ($matches as $key => $match) {
+                    if (is_string($key)) {
+                        $params[$key] = $match;
+                    }
+                }
+
                 $this->params = $params;
                 return true;
             }
-        }
-        */
-
-        // Match to the fixed URL format /controller/action
-        $reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
-
-        if (preg_match($reg_exp, $url, $matches)) {
-            // Get named capture group values
-            $params = [];
-
-            foreach ($matches as $key => $match) {
-                if (is_string($key)) {
-                    $params[$key] = $match;
-                }
-            }
-
-            $this->params = $params;
-            return true;
         }
 
         return false;
--- public/index.php
+++ public/index.php
@@ -16,12 +16,14 @@ $router = new Router();
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
-$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+$router->add('{controller}/{action}');
+$router->add('{controller}/{id:\d+}/{action}');
     
 // Display the routing table
-//echo '<pre>';
+echo '<pre>';
 //var_dump($router->getRoutes());
-//echo '</pre>';
+echo htmlspecialchars(print_r($router->getRoutes(), true));
+echo '</pre>';
 
 
 // Match the requested route
