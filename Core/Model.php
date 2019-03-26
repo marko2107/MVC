38946ea8c35d28e0229a638783ff739fe3525081
marko2107 <marko.drnasin@gmail.com>
2019-03-26 21:07:12 +0100

    dodan autoload, config files, models

---
+++ Core/Model.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Core;
+
+use PDO;
+use App\Config;
+
+/**
+ * Base model
+ *
+ * PHP version 5.4
+ */
+abstract class Model
+{
+
+    /**
+     * Get the PDO database connection
+     *
+     * @return mixed
+     */
+    protected static function getDB()
+    {
+        static $db = null;
+
+        if ($db === null) {
+            //$host = 'localhost';
+            //$dbname = 'mvc';
+            //$username = 'root';
+            //$password = 'secret';
+    
+            try {
+                //$db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8",
+                //              $username, $password);
+                $dsn = 'mysql:host=' . Config::DB_HOST . ';dbname=' . 
+                       Config::DB_NAME . ';charset=utf8';
+                $db = new PDO($dsn, Config::DB_USER, Config::DB_PASSWORD);
+
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        }
+
+        return $db;
+    }
+}
