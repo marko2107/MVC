328e6042ba61c1806651ab4222166109b63dd2d0
marko2107 <marko.drnasin@gmail.com>
2019-03-26 19:32:26 +0100

    dodan twig template engine

--- App/Controllers/Posts.php
+++ App/Controllers/Posts.php
@@ -1,11 +1,13 @@
 <?php
 
+namespace App\Controllers;
+
 /**
  * Posts controller
  *
  * PHP version 5.4
  */
-class Posts
+class Posts extends \Core\Controller
 {
 
     /**
@@ -13,7 +15,7 @@ class Posts
      *
      * @return void
      */
-    public function index()
+    public function indexAction()
     {
         echo 'Hello from the index action in the Posts controller!';
     }
@@ -23,8 +25,20 @@ class Posts
      *
      * @return void
      */
-    public function addNew()
+    public function addNewAction()
     {
         echo 'Hello from the addNew action in the Posts controller!';
+    }
+    
+    /**
+     * Show the edit page
+     *
+     * @return void
+     */
+    public function editAction()
+    {
+        echo 'Hello from the edit action in the Posts controller!';
+        echo '<p>Route parameters: <pre>' .
+             htmlspecialchars(print_r($this->route_params, true)) . '</pre></p>';
     }
 }
